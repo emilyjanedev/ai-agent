b029c68d17829c3b78ce0345f662b744677bc19e
Emily Schur <ejschur@gmail.com>
2025-07-14 13:28:49 -0700

    Store user prompt in list for message history

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import sys
-from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+from dotenv import load_dotenv
 
 def main():
     load_dotenv()
@@ -18,12 +19,14 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
+    generate_content(client, messages)
+
+def generate_content(client, messages):
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents=[user_prompt]
+        model="gemini-2.0-flash-001",
+        contents=messages,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
