5ebcaa2822d6efcf70066764638678484cb66756
Emily Schur <ejschur@gmail.com>
2025-07-16 13:55:07 -0700

    Add run_python functionality

--- tests.py
+++ tests.py
@@ -1,17 +1,27 @@
-from functions.write_file_content import write_file
+from functions.run_python import run_python_file
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result = run_python_file("calculator", "main.py")
     print("Result: ")
     print(result)
     print("")
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
     print("Result: ")
     print(result)
     print("")
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    result = run_python_file("calculator", "tests.py")
+    print("Result: ")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "../main.py")
+    print("Result: ")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "nonexistent.py")
     print("Result: ")
     print(result)
     print("")
